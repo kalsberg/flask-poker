a7866844d4105dc921a41fb41f12cd14ae7fb71a
Kalyani Soni <46649552+kalsberg@users.noreply.github.com>
2024-05-27 19:08:43 +0200

    Update game.js

--- app/static/game.js
+++ app/static/game.js
@@ -1,89 +1,47 @@
-function createPlayer(id, name, vote) {
-    let rowId = "playerRow_" + id;
-    let playerRow = document.getElementById(rowId);
-    
-    if (playerRow) {
-        return playerRow;
+// Event listeners
+document.addEventListener("DOMContentLoaded", function () {
+
+    var copyInviteButton = document.getElementById("copyInvite")
+    if (copyInviteButton) {
+        copyInviteButton.addEventListener("click", function(event) {
+            var pageUrl = window.location.href;
+            navigator.clipboard.writeText(pageUrl)
+                .then(function() {
+                    copyInviteButton.innerText = 'COPIED!';
+                
+                setTimeout(function() {
+                    copyInviteButton.innerText = 'Copy invite link';
+                }, 2000);
+            })
+            .catch(function(error) {
+                console.error("Failed to copy: ", error);
+            });
+        })
     }
-
-    playerRow = document.createElement("tr");
-    playerRow.id = rowId;
-    playerRow.className = "border-b border-neutral-200 dark:border-white/10";
-
-    let nameData = document.createElement("td");
-    nameData.className = "whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10";
-    nameData.textContent = name;
+})
     
-    let voteData = document.createElement("td");
-    voteData.className = "whitespace-nowrap border-e border-neutral-200 px-6 py-4 dark:border-white/10";
-    
-    if (vote) {
-        let voteValue = document.createElement("span");
-        voteValue.className = "vote-value";
-        voteValue.style.display = "none";
-        voteValue.textContent = vote;
-
-        let voteStatus = document.createElement("span");
-        voteStatus.className = "vote-status";
-        voteStatus.textContent = "Voted";
-
-        voteData.appendChild(voteValue);
-        voteData.appendChild(voteStatus);
-    } else {
-        voteData.textContent = "Yet to vote";
-    }
-
-    playerRow.appendChild(nameData);
-    playerRow.appendChild(voteData);
-    
-    let tbody = document.querySelector("tbody");
-    let revealVotesRow = document.getElementById("revealVotesRow");
-    tbody.insertBefore(playerRow, revealVotesRow);
-    
-    return playerRow;
-}
-
-
-function copyToClipboard() {
-    var pageUrl = window.location.href;
-
-    navigator.clipboard.writeText(pageUrl)
-        .then(function() {
-        document.getElementById('copy-invite').innerText = 'COPIED!';
-        
-        setTimeout(function() {
-            document.getElementById('copy-invite').innerText = 'Copy invite link';
-        }, 2000);
-    })
-    .catch(function(error) {
-        console.error("Failed to copy: ", error);
-    });
-}
 
 window.onload = () => {
     const socket = io();
     socket.io.opts.transports = ["websocket"];
-    let averageVote = 0;
 
-    socket.on("vote", votes => {
-        let totalVotes = 0;
-        let voteCount = votes.length;
+    // socket.on("vote", votes => {
+    //     let totalVotes = 0;
+    //     let voteCount = votes.length;
     
-        votes.forEach(vote => {
-            let row = createPlayer(vote['user']);
-            row.getElementsByTagName("td")[0].innerText = vote['name'];
-            row.getElementsByTagName("td")[1].innerText = vote['value'];
+    //     votes.forEach(vote => {
+    //         let row = createPlayer(vote['user']);
+    //         row.getElementsByTagName("td")[0].innerText = vote['name'];
+    //         row.getElementsByTagName("td")[1].innerText = vote['value'];
     
-            totalVotes += parseFloat(vote['value']);
-        });
+    //         totalVotes += parseFloat(vote['value']);
+    //     });
     
-        let averageVote = totalVotes / voteCount;
-        let averageVoteText = `CONSENSUS: ${averageVote.toFixed(2)}`;
-        console.log(averageVoteText);
+    //     let averageVote = totalVotes / voteCount;
+    //     let averageVoteText = `CONSENSUS: ${averageVote.toFixed(2)}`;
     
-        // Update the average vote in the HTML
-        document.getElementById("averageVoteCell").innerText = averageVoteText;
-    });
+    //     document.getElementById("averageVoteCell").innerText = averageVoteText;
+    // });
     
     const voteButtons = document.getElementById("vote").getElementsByTagName("button");
     for (let button of voteButtons) {
